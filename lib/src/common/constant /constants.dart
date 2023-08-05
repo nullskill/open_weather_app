77ca9c6c53613ae111c2dd8ad7129411e45e300d
nullskill <22912338+nullskill@users.noreply.github.com>
2023-08-05 16:27:06 +0300

    Добавлены константы для API

---
+++ lib/src/common/constant /constants.dart
@@ -0,0 +1,12 @@
+class Constants {
+  /// Константы для API
+  static const bundleId = 'ru.larkin.open_weather_app';
+
+  static const String protocol = 'https://';
+
+  static const String hostApi = 'api.openweathermap.org/data';
+  static const String baseUrlApi = protocol + hostApi;
+  static const String weatherApiVersion = '2.5';
+  static const String oneCallApiVersion = '3.0';
+  static const String apiKey = '';
+}
